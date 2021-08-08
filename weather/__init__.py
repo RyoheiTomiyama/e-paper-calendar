954b123d62e6c7b8674a8363040532c93fa4647b
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2021-08-08 12:46:25 +0900

    fix weather info because min/max is incorrected

--- weather/__init__.py
+++ weather/__init__.py
@@ -5,8 +5,10 @@ from decimal import Decimal
 
 load_dotenv()
 
-BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall'
 API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
+LAT = 36.5
+LONG = 137.8667
 
 class Temp:
   current: Decimal
@@ -26,15 +28,15 @@ class Weather():
     if API_KEY is None:
       print('OPEN_WEATHER_API_KEY is not found')
       return
-    preload = { 'id': 1854186, 'units': 'metric', 'appid': API_KEY }
+    preload = { 'lat': LAT, 'lon': LONG, 'units': 'metric', 'exclude': 'minutely,hourly,alert', 'appid': API_KEY }
     r = requests.get(BASE_URL, params = preload)
     data = r.json()
     print(r.json())
-    self.weather = data['weather'][0]['main']
-    self.icon = data['weather'][0]['icon']
-    self.temp.current = Decimal(str(data['main']['temp'])).quantize(Decimal('0.1'))
-    self.temp.min = Decimal(str(data['main']['temp_min'])).quantize(Decimal('0.1'))
-    self.temp.max = Decimal(str(data['main']['temp_max'])).quantize(Decimal('0.1'))
-    self.pressure = data['main']['pressure']
-    self.humidity = data['main']['humidity']
-    self.wind = data['wind']['speed']
+    self.weather = data['current']['weather'][0]['main']
+    self.icon = data['current']['weather'][0]['icon']
+    self.temp.current = Decimal(str(data['current']['temp'])).quantize(Decimal('0.1'))
+    self.temp.min = Decimal(str(data['daily'][0]['temp']['min'])).quantize(Decimal('0.1'))
+    self.temp.max = Decimal(str(data['daily'][0]['temp']['max'])).quantize(Decimal('0.1'))
+    self.pressure = data['current']['pressure']
+    self.humidity = data['current']['humidity']
+    self.wind = data['current']['wind_speed']
